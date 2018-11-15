352af2b03b87740de10d64f481b17c4e16a01115
Lai.gongqi <it.laigq@gmail.com>
2018-11-15 16:01:00 +0800

    更新-v0.1.0静态资源promise改进、引入Handlebars、(对应6.5-6.6)

--- src/app.js
+++ src/app.js
@@ -72,4 +72,8 @@ server.listen(conf.port, conf.hostname, () => {
  * 6.1
  * 6.2
  * 6.3
+ * 6.4
+ *
+ * 6.5
+ * 6.6
  */
--- src/helper/route.js
+++ src/helper/route.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
+const path = require('path')
+const Handlebars = require('handlebars')
 const promisity = require('util').promisify;
 const stat = promisity(fs.stat)
 const readdir = promisity(fs.readdir)
+const config = require('../config/defaultConfig')
+
+const tplPath = path.join(__dirname, '../template/dir.html')
+const source = fs.readFileSync(tplPath)
+// const source = fs.readFileSync(tplPath,'utf-8')
+
+const template = Handlebars.compile(source.toString())
+// const source =fs.readFileSync('../template/dir.html')
 
 module.exports = async function (request, respond, filePath) {
     try {
@@ -14,8 +24,17 @@ module.exports = async function (request, respond, filePath) {
             const files = await readdir(filePath)
 
             respond.statusCode = 200;
-            respond.setHeader('Content-Type', 'text/plain')
-            respond.end(files.join(','))
+            respond.setHeader('Content-Type', 'text/html')
+            const dir = path.relative(config.root, filePath)
+            const data = {
+                title: path.basename(filePath),
+                dir: dir ? `/${dir}` : '',
+                files
+            }
+
+            // console.log(data)
+            respond.end(template(data))
+            // respond.end(files.join(','))
         }
     } catch (e) {
         respond.statusCode = 404;
